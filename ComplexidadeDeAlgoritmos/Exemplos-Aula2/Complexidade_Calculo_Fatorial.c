5d26d4de4a8a396ae33b42e2948b276a0c654f4a
GabrielFerrante <gabrielsferrante@hotmail.com>
2025-08-29 19:25:18 -0300

    Euler exponencial

--- ComplexidadeDeAlgoritmos/Exemplos-Aula2/Complexidade_Calculo_Fatorial.c
+++ ComplexidadeDeAlgoritmos/Exemplos-Aula2/Complexidade_Calculo_Fatorial.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
-
+#include <math.h>
 
 long long int fat(int n){
 	if(n==1){
@@ -23,6 +23,28 @@ long long int fat(int n){
 //T(n) = n
 //Big O(n)
 
+float euler_exponencial(x, n){ //x é o expoente, e n é o limite superior 
+	if(n==0){
+		return 0;
+	}else{
+		return (pow(x, n)/fat(n))+euler_exponencial(x, n-1);
+	}
+}
+
+//Usando a regra da recorrência
+//T(n) = T(n-1) + O(1) + O(N), note que, O(1) é a soma e O(N) é a complexidade da função fat
+//T(n) = T(n-1) + O(N), note que o O(1) desaparece pois é utilizado somente o maior.
+//Logo temos a seguinte abrimento:
+//T(n) = T(n-1) + c.n, note que c.n, é igual a O(N)
+//T(n) = T(n-2) + c.(n-1) + c.n
+//T(n) = T(n-3) + c.(n-2) + c.(n-1) + c.n
+//Para um K arbitrário:
+//T(n) = T(n-k) + c.[1 + 2 + 3 + ...+ n], note que é igual uma soma de P.A, ou seja, n.(n+1)/2
+//Na condição de parada:
+//n-k = 0, logo, k tem que ser igual a n
+//T(n) = T(0) (ou seja 1) + c . n(n+1)/2
+//T(n) = 1 + c. (n² + n)/2 
+//Ficando Big O(n²)
 
 void main(){
 	
